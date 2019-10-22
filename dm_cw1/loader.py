18a2192405335bfb1df77ce90b1515106e8d6545
QDucasse <quentin.ducasse@ensta-bretagne.org>
2019-10-22 18:04:19 +0100

    2, 5 and 10 best features selected. Datasets generation working

--- dm_cw1/loader.py
+++ dm_cw1/loader.py
@@ -147,13 +147,74 @@ def store_best_abs_corr_attributes(nb):
     signs_1label = load_boolean_mask(nb)
     cor = signs_1label.corr()
     cor_target = abs(cor["label"])
-    relevant_features = cor_target[cor_target>0.3].sort_values(ascending="False")
+    relevant_features = cor_target[cor_target>0.01].sort_values(ascending=False)
     relevant_features.to_csv('./data/best_features_smpl_' + str(nb) + '.csv')
+    return relevant_features.head(n=10)
 
-store_best_abs_corr_attributes(3)
+def store_best_attributes():
+    for i in range(10):
+        print("Finding best attributes for " + str(i))
+        store_best_abs_corr_attributes(i)
 
 # Print heatmap
 # plt.figure(figsize=(12,10))
 # print(signs_label3['label'].corr(signs_label3['0']))
 # sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
 # plt.show()
+
+# From the created files
+# ba = best atributes
+
+# Hardcoded values
+ba0 = [1172, 1171, 1468, 1220, 1472, 1221,
+       1123, 1469, 1419, 1519, 1124, 1471]
+
+ba1 = [1094, 1046, 1172, 1142,  998, 1190,
+       1173,  997,  981, 1045,  950, 1143]
+
+ba2 = [1084, 1132, 1083, 1131, 1082, 1130,
+       1036, 1081, 1179, 1035, 1129, 1178]
+
+ba3 = [1696, 1697, 1648, 1649, 1695, 1698,
+       1745, 1744, 1713, 1712, 1647, 1650]
+
+ba4 = [1849, 1850, 1848, 1897, 1801, 1802,
+       1898, 1800, 1896, 1043, 1847, 1309]
+
+ba5 = [1319, 1367, 1271, 1368, 1270, 1318,
+       1320, 1415, 1416, 1222, 1366, 1223]
+
+ba6 = [1186, 1738, 1934, 1737, 1786, 1787,
+       1885, 1689, 1983, 1688,  633, 1836]
+
+ba7 = [1168, 1120, 1169, 1119, 1167, 1121,
+       1216, 1072, 1071, 1215, 1217, 1118]
+
+ba8 = [1280, 1232, 1328, 1184, 1375, 1376,
+       1327, 1470, 1423, 1422, 1471, 1469]
+
+ba9 = [1362, 1410, 1363, 1364, 1411, 1365,
+       1412, 1317, 1318, 1413, 1361, 1314]
+
+# Create datasets with the n best features
+
+def best_n_attributes(nb):
+    global ba0,ba1,ba2,ba3,ba4,ba5,ba6,ba7,ba8,ba9
+    ba_n = ba0[:nb] + ba1[:nb] + ba2[:nb] + ba3[:nb] + ba4[:nb] \
+            + ba5[:nb] + ba6[:nb] + ba7[:nb] + ba8[:nb] + ba9[:nb]
+    ba_n = [str(i) for i in ba_n]
+    return ba_n
+
+ba_n2  = best_n_attributes(2)
+ba_n5  = best_n_attributes(5)
+ba_n10 = best_n_attributes(10)
+
+signs_ba2  = signs[ba_n2]
+print(signs_ba2)
+signs_ba2.to_csv('./data/x_train_gr_smpl_2ba.csv')
+signs_ba5  = signs[ba_n5]
+print(signs_ba5)
+signs_ba5.to_csv('./data/x_train_gr_smpl_5ba.csv')
+signs_ba10 = signs[ba_n10]
+print(signs_ba10)
+signs_ba10.to_csv('./data/x_train_gr_smpl_10ba.csv')
