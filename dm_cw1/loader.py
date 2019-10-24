edef3b65fa620422e95762d31762a74da109f2ab
QDucasse <quentin.ducasse@ensta-bretagne.org>
2019-10-24 15:05:34 +0100

    kmeans beginning

--- dm_cw1/loader.py
+++ dm_cw1/loader.py
@@ -240,13 +240,63 @@ signs_ba10,signs_ba10_rd = dataset_best_n_attributes(10)
 # signs_ba10.to_csv('./data/x_train_gr_smpl_10ba.csv')
 
 # Run Bayes over the new sets
-gaussian_train_test(signs_ba2_rd,'label')
-gaussian_train_test(signs_ba5_rd,'label')
-gaussian_train_test(signs_ba10_rd,'label')
+# gaussian_train_test(signs_ba2_rd,'label')
+# gaussian_train_test(signs_ba5_rd,'label')
+# gaussian_train_test(signs_ba10_rd,'label')
 
 # ============================================
 #            K-MEANS CLUSTERING
 # ============================================
 
+# Unsupervised -> Drop 'label' field
+signs_ba2_rd_nolab = signs_ba2_rd.drop('label',axis = 1)
 signs_ba2_rd_std = normalize_dataset(signs_ba2_rd)
-print(signs_ba2_rd_std)
+
+signs_ba5_rd_nolab = signs_ba5_rd.drop('label',axis = 1)
+signs_ba5_rd_std = normalize_dataset(signs_ba5_rd)
+
+signs_ba10_rd_nolab = signs_ba10_rd.drop('label',axis = 1)
+signs_ba10_rd_std = normalize_dataset(signs_ba10_rd)
+
+def plot_elbow(df,nb_clusters = 11):
+    wcss = []
+    for i in range(1, nb_clusters):
+        kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+        kmeans.fit(df)
+        wcss.append(kmeans.inertia_)
+    plt.plot(range(1, 11), wcss)
+    plt.title('The Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+# plot_elbow(signs_ba2_rd_std)
+# # found 3/4 clusters
+# plot_elbow(signs_ba5_rd_std)
+# # found 3/4 clusters
+# plot_elbow(signs_ba10_rd_std)
+# # found 3/4 clusters
+
+n_clusters_elbow = 3
+n_clusters = 10
+
+def unsup_kmeans_train_test(df,df_std,n_clusters):
+    # Fitting K-Means to the dataset
+    kmeans = KMeans(n_clusters = n_clusters, init = 'k-means++', random_state = 42)
+    y_kmeans = kmeans.fit_predict(df_std)
+    #beginning of  the cluster numbering with 1 instead of 0
+    y_kmeans1=y_kmeans
+    y_kmeans1=y_kmeans+1
+    # New Dataframe called cluster
+    cluster = pd.DataFrame(y_kmeans1)
+    # Adding cluster to the Dataset1
+    df['cluster'] = cluster
+    #Mean of clusters
+    kmeans_mean_cluster = pd.DataFrame(round(df.groupby('cluster').mean(),1))
+    return kmeans_mean_cluster
+
+kmeans_cluster = KMeans(n_clusters = 10)
+kmeans_cluster.fit(signs_ba5_rd_std)
+print()
+print(kmeans_cluster.labels_[::10])
+print("KMeans accuracy : ", accuracy_score(signs_ba5_rd['label'],kmeans_cluster.labels_, normalize = True))
